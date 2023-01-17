1d4d3d5a641bb51e83f614d3406ebbe72d7dba43
tuan-trankhac <tuan.tk200554@sis.hust.edu.vn>
2023-01-17 21:51:15 +0700

    Cultural Festival

---
+++ src/main/java/crawl/CulturalFestivalCrawl.java
@@ -0,0 +1,130 @@
+package crawl;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import util.JsonHandler;
+import util.ListURL;
+
+import java.io.IOException;
+import java.util.*;
+
+public class CulturalFestivalCrawl {
+    private final String fileData = "data/CulturalFestival.json";
+    private String source;
+    private static int qty = 0;
+    private static String time;
+    private static String name;
+    private static String location;
+    private static String firstTime;
+    private static List<String> relatedCharacter = new ArrayList<>();
+    private static int relatedHistoricalSites;
+    private static String summary;
+
+
+    public CulturalFestivalCrawl(String source) {
+        this.source = source;
+    }
+
+    public void getData() {
+        List<String> url = ListURL.getURL(source);
+        JSONArray arr = new JSONArray();
+        assert url != null;
+        for (String s : url) {
+            try {
+                Document document = Jsoup.connect(s).get();
+                Elements table = document.getElementsByClass("prettytable wikitable");
+                Elements elements = Objects.requireNonNull(table.first()).getElementsByTag("td");
+                for (int j = 2; j < elements.size(); j+=6) {
+                    time = getTime(elements.get(j));
+                    location = getLocation(elements.get(j+1));
+                    name = getName(elements.get(j+2));
+                    summary = getSummary(elements.get(j+2));
+                    firstTime = getFirstTime(elements.get(j+3));
+                    relatedCharacter = getRelatedCharacter(elements.get(j+4));
+//                    relatedHistoricalSites
+                    ++qty;
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        JsonHandler.writeJsonFile(arr, fileData);
+    }
+
+    private List<String> getRelatedCharacter(Element element) {
+        List<String> relatedCharacter = new ArrayList<>();
+        String a = element.select("td").text();
+        Collections.addAll(relatedCharacter, a.split(", ", 0));
+        return relatedCharacter;
+    }
+
+
+
+    private String getTime(Element element) {
+        return element.select("td").text();
+    }
+    private String getFirstTime(Element element) {
+        return element.select("td").text();
+    }
+    public static String getLocation(Element element) {
+        return element.select("td").text();
+    }
+
+    private String getName(Element element) {
+        return element.select("td").text();
+    }
+
+    private  String getSummary(Element element)  {
+        StringBuilder summary = new StringBuilder();
+        try {
+            Elements a = element.select("a");
+            if (a.size() > 1) return null;
+            String url = a.attr("href");
+            Document document = Jsoup.connect("https://vi.wikipedia.org" + url).get();
+            Element p = document.select(".mw-parser-output > p").first();
+            while (true) {
+                assert p != null;
+                if (!p.tagName().equals("p")) break;
+                summary.append(p.text());
+                p = p.nextElementSibling();
+            }
+
+        }catch (Exception ignored){
+
+        }
+        return summary.toString();
+    }
+
+    public JSONObject getCulturalFestival() {
+        JSONObject obj = new JSONObject();
+        obj.put("id", "Cultural Festival " + qty);
+        obj.put("Cultural Festival", name);
+        obj.put("Time", time);
+        obj.put("Location", location);
+        obj.put("First Time", firstTime);
+        obj.put("Related Character", relatedCharacter);
+        obj.put("Related Historical Sites", relatedHistoricalSites);
+        obj.put("Summary", summary);
+        System.out.println(obj);
+        return obj;
+    }
+    public JSONArray getCulturalFestival(Elements elements) {
+        JSONArray jsonArray = new JSONArray();
+        for (Element element : elements) {
+            time = getTime(element);
+            location = getLocation(element);
+            firstTime = getFirstTime(element);
+            name = getName(element);
+            relatedCharacter = getRelatedCharacter(element);
+//            relatedHistoricalSites = getRelatedHistoricalSites();
+            summary = getSummary(element);
+            jsonArray.add(getCulturalFestival());
+        }
+        return jsonArray;
+    }
+
+}
--- src/main/java/test/MainTest.java
+++ src/main/java/test/MainTest.java
@@ -1,10 +1,13 @@
 package test;
 
+import crawl.CulturalFestivalCrawl;
 import crawl.DynastyCrawl;
 
 public class MainTest {
      public static void main(String[] args){
-          DynastyCrawl obj = new DynastyCrawl( "url/DynastyURL.txt");
+//          DynastyCrawl obj = new DynastyCrawl( "url/DynastyURL.txt");
+//          obj.getData();
+          CulturalFestivalCrawl obj = new CulturalFestivalCrawl( "url/CulturalFestival.txt");
           obj.getData();
      }
 }
